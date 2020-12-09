4af5e10fef6324729f1f31318c727fe80178b435
Yin <yinnnn17@gmail.com>
2020-12-09 15:35:45 -0800

    Finished Challenge4

---
+++ src/Components/MetalMeta/Band.js
@@ -0,0 +1,21 @@
+// /Components/MetalMeta/Band.js
+
+import React from 'react'
+
+function Band(props) {
+  const {band_name,formed,origin,fans} = props
+  return (
+    <div className="Band">
+      <h4>{band_name}</h4>
+      <p>
+        <b> Origin</b>:{origin}  
+        <b> Fans</b>:{fans} 
+        <b> Formed</b>:{formed}
+      </p>
+      
+    </div>
+    
+  )
+}
+
+export default Band
