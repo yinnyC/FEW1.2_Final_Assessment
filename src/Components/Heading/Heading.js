6ef2795c4f3f6999fb84cfe7e47ce9f012419d08
Yin <yinnnn17@gmail.com>
2020-12-09 15:10:48 -0800

    Finished Challenge2

---
+++ src/Components/Heading/Heading.js
@@ -0,0 +1,17 @@
+// src/Heading.js
+
+import React from 'react'
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <header>
+      <h1>FEW 1.2 Final Assessment</h1>
+      <h2>Yin Chang</h2>
+      </header>
+    </div>
+    
+  )
+}
+
+export default Heading
